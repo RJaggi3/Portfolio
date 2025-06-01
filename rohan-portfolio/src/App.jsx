26efdac0eb208647544cc25ed1f9196ab6ecc3c8
rjaggi3 <rohanjaggi2003@gmail.com>
2025-06-01 19:58:15 +1000

    Created the navbar

---
+++ rohan-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+/**
+ * Description:
+ * Component for the home page.
+ */
+
+  return (
+  <div>
+    <Navbar/>
+    <div id="content">
+      <div id="about"></div>
+    </div>
+  </div> 
+  )
+}
+
+export default App
---
+++ rohan-portfolio/src/Navbar.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './Navbar.css'
+
+function Navbar() {
+/**
+ * Description:
+ * Component for the navbar.
+ */
+
+  return (
+  <div id="navbar">
+    <div id="navbar-items">
+        <span id="text">Rohan</span>
+        <span>
+            <span id="text">Projects</span>
+            <span id="text">Experience</span>
+        </span>
+    </div>
+    
+  </div>
+  )
+}
+
+export default Navbar
